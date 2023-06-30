2157764710e8cdce5bdb3d23056e48a1eb9c8cbc
narapady <chhuoynarapady@yahoo.com>
2023-06-30 14:40:59 -0700

    second commit

--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
 import os
 
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from tabulate import tabulate
 
 load_dotenv()
 
@@ -10,7 +12,8 @@ load_dotenv()
 titles = []
 companies = []
 locations = []
-job_details = []
+salaries = []
+work_types = []
 
 
 def get_soup(start_at: int) -> BeautifulSoup:
@@ -18,35 +21,58 @@ def get_soup(start_at: int) -> BeautifulSoup:
     payload = {"api_key": os.getenv("API_KEY"), "url": URL, "dynamic": "false"}
     resp = requests.get("https://api.scrapingdog.com/scrape", params=payload)
     soup = BeautifulSoup(resp.text, "html.parser")
+
     return soup
 
 
 def extract(soup: BeautifulSoup) -> None:
     jobs = soup.find_all("td", "resultContent")
     for job in jobs:
-        title = job.find("h2").a.span["title"]
-        company = job.find("span", class_="companyName").text
-        location = job.find("div", class_="companyLocation").text
-        job_detail = [word for word in job.contents[2].strings]
+        title = job.find("h2")
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
 
-        titles.append(title)
-        companies.append(company)
-        locations.append(location)
-        job_details.append(job_detail)
+        if title and company and location:
+            titles.append(title.a.span["title"])
+            companies.append(company.text)
+            locations.append(location.text)
 
-    # titles = [job.find("h2").a.span["title"] for job in jobs]
-    # companies = [job.find("span", class_="companyName").text for job in jobs]
-    # locations = [job.find("div", class_="companyLocation").text for job in jobs]
-    # job_details = [[word for word in job.contents[2].strings] for job in jobs]
+        for word in job.contents[2].strings:
+            if "$" in word:
+                salaries.append(word)
+            else:
+                salaries.append("unknown")
+
+            if word.lower() in ("full-time", "contract", "part-time"):
+                work_types.append(word.lower())
+            else:
+                work_types.append("unknown")
 
 
 def main() -> None:
     soup = get_soup(start_at=0)
     extract(soup)
-    print(titles)
-    print(companies)
-    print(locations)
-    print(job_details)
+    # df = pd.DataFrame(
+    #     {
+    #         "job_title": titles,
+    #         "company_name": companies,
+    #         "location": locations,
+    #         "salary": salaries,
+    #         "work_type": work_types,
+    #     }
+    # )
+
+    # print(tabulate(df, headers="keys", tablefmt="psql"))
+    print(len(titles))
+    print(titles, "\n\n")
+    print(len(companies))
+    print(companies, "\n\n")
+    print(len(locations))
+    print(locations, "\n\n")
+    print(len(salaries))
+    print(salaries)
+    print(len(work_types))
+    print(work_types)
 
 
 if __name__ == "__main__":
