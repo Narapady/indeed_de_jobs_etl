15437bb6dd1278f64f96c2a37901bc66d760e631
narapady <chhuoynarapady@yahoo.com>
2023-07-06 18:10:36 -0700

    fixed tranformation

--- tranformer.py
+++ tranformer.py
@@ -4,6 +4,68 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York State": "NY",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington State": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "Washington, DC": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "Virgin Islands": "VI",
+}
+
 
 def get_zipcode(location: str) -> str:
     zipcode = re.findall(r"\d+", location)
@@ -17,79 +79,37 @@ def get_city(location: str) -> str:
     city = location.split(", ")[0]
 
     if not city:
-        return "unknown"
+        city = "unknown"
 
-    if "Remote in " in city:
+    elif len(location.split(", ")) == 1:
+        city = "unknown"
+
+    elif "Washington, DC" in location:
+        city = "Washington, DC"
+
+    elif "Remote in " in city:
         city = city.replace("Remote in ", "")
 
-    if "Hybrid remote in " in city:
+    elif "Hybrid remote in " in city:
         city = city.replace("Hybrid remote in ", "")
+
+    elif city.strip() == "Remote":
+        city = "unknown"
 
     return city.strip()
 
 
 def get_state(location: str) -> str:
-    states = [
-        "IA",
-        "KS",
-        "UT",
-        "VA",
-        "NC",
-        "NE",
-        "SD",
-        "AL",
-        "ID",
-        "FM",
-        "DE",
-        "AK",
-        "CT",
-        "PR",
-        "NM",
-        "MS",
-        "PW",
-        "CO",
-        "NJ",
-        "FL",
-        "MN",
-        "VI",
-        "NV",
-        "AZ",
-        "WI",
-        "ND",
-        "PA",
-        "OK",
-        "KY",
-        "RI",
-        "NH",
-        "MO",
-        "ME",
-        "VT",
-        "GA",
-        "GU",
-        "AS",
-        "NY",
-        "CA",
-        "HI",
-        "IL",
-        "TN",
-        "MA",
-        "OH",
-        "MD",
-        "MI",
-        "WY",
-        "WA",
-        "OR",
-        "MH",
-        "SC",
-        "IN",
-        "LA",
-        "MP",
-        "DC",
-        "MT",
-        "AR",
-        "WV",
-        "TX",
-    ]
+    states = list(us_state_to_abbrev.values())
+    state = location.split(", ")
+    if len(state) == 1 and "Remote in " in state[0]:
+        state = state[0].replace("Remote in ", "")
+        return us_state_to_abbrev[state]
+
+    if len(state) == 1 and "Hybrid remote in " in state[0]:
+        state = state[0].replace("Hybrid remote in ", "")
+        return us_state_to_abbrev[state]
+
     regex = re.compile(r"\b(" + "|".join(states) + r")\b")
     state = re.findall(regex, location)
 
@@ -103,6 +123,14 @@ def tranform_work_hour(work_hour: str) -> str:
     if work_hour == "unknown":
         return "unknown"
     return work_hour
+
+
+def get_work_type(location: str) -> str:
+    if "Hybrid remote in" in location:
+        return "Hybrid remote"
+    if "Remote in" in location:
+        return "Remote"
+    return "On site"
 
 
 def tranform_salary(salary: str) -> str:
@@ -120,7 +148,7 @@ def main() -> None:
     df["zipcode"] = df["location"].apply(lambda x: get_zipcode(x))
     df["work_hour"] = df["work_hour"].apply(lambda x: tranform_work_hour(x))
     df["salary"] = df["salary"].apply(lambda x: tranform_salary(x))
-
+    df["work_type"] = df["location"].apply(lambda x: get_work_type(x))
     df = df.rename(
         columns={"salary": "estimated_salary_usd", "location": "company_address"}
     )
