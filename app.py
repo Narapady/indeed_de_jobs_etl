95e8dc1fc571ae741d2090a2d035e73c5fed17f2
narapady <chhuoynarapady@yahoo.com>
2023-07-18 20:24:07 -0700

    add pie charts

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # - to activate conda environment: conda activate py38_env
 
 import folium
+import plotly.express as px
 import streamlit as st
 from dotenv import load_dotenv
 from snowflake.snowpark import Session
@@ -35,7 +36,7 @@ def load_data(table_name):
 
     ## Collect the results. This will run the query and download the data
     table = table.collect()
-    return table
+    st.dataframe(table)
 
 
 @st.cache_data
@@ -53,7 +54,7 @@ def load_map(table_name):
 
 
 def folium_map(table_name: str) -> None:
-    st.write("Map of data engineering jobs in the US")
+    st.header("Map of data engineering jobs in the US")
     lat_lon_df = (
         session.table(f"{table_name}")
         .select(col("CITY"), col("LATITUDE"), col("LONGITUDE"))
@@ -63,7 +64,6 @@ def folium_map(table_name: str) -> None:
     lat_lon_df = lat_lon_df.astype({"LATITUDE": "float32", "LONGITUDE": "float32"})
     lat_lon_df.drop_duplicates()
 
-    # center on Liberty Bell, add marker
     lat_lon_tuples = list(
         zip(
             lat_lon_df["CITY"].to_list(),
@@ -72,7 +72,7 @@ def folium_map(table_name: str) -> None:
         )
     )
 
-    m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
+    m = folium.Map(location=[39.909726, -100.785153], zoom_start=4)
     for city, lat, long in lat_lon_tuples:
         folium.Marker([lat, long], popup=city, tooltip=city).add_to(m)
 
@@ -80,13 +80,44 @@ def folium_map(table_name: str) -> None:
     st_data = st_folium(m, width=725)
 
 
+def load_piechart_state(table_name: str) -> None:
+    st.header("Top 10 US states with most data engineering jobs")
+    sql_str = f"SELECT state,\
+                COUNT(*) de_jobs_by_state\
+                FROM {table_name}\
+                WHERE state <> 'unknown'\
+                GROUP BY state\
+                ORDER BY COUNT(*) DESC\
+                LIMIT 10;\
+                "
+    df = session.sql(sql_str).collect()
+    fig = px.pie(df, values="DE_JOBS_BY_STATE", names="STATE")
+    st.plotly_chart(fig)
+
+
+def load_piechart_city(table_name: str) -> None:
+    st.header("Top 10 US cities with most data engineering jobs")
+    sql_str = f"SELECT city,\
+                COUNT(*) de_jobs_by_city\
+                FROM {table_name}\
+                WHERE state <> 'unknown'\
+                GROUP BY city\
+                ORDER BY COUNT(*) DESC\
+                LIMIT 10;\
+                "
+    df = session.sql(sql_str).collect()
+    fig = px.pie(df, values="DE_JOBS_BY_CITY", names="CITY")
+    st.plotly_chart(fig)
+
+
 def main() -> None:
     table_name = "indeed_de_jobs_us"
-    with st.expander("See Table"):
-        df = load_data(table_name)
-        st.dataframe(df)
-
+    with st.sidebar:
+        st.write("This is side bar")
+    load_data(table_name)
     folium_map(table_name)
+    load_piechart_state(table_name)
+    load_piechart_city(table_name)
 
 
 if __name__ == "__main__":
