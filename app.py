95524f8dbeebd90a4f36e96342dc6a18985371b0
narapady <chhuoynarapady@yahoo.com>
2023-07-18 18:47:55 -0700

    add folium map

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-import os
+# - to activate conda environment: conda activate py38_env
 
+import folium
 import streamlit as st
 from dotenv import load_dotenv
 from snowflake.snowpark import Session
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.types import DoubleType
+from streamlit_folium import st_folium
 
 load_dotenv()
 
-st.title("❄️ How to connect Streamlit to a Snowflake database")
+st.title("Data Engineering Jobs in the US")
 
 
 # Establish Snowflake session
@@ -34,7 +38,56 @@ def load_data(table_name):
     return table
 
 
-table_name = "indeed_de_jobs_us"
-with st.expander("See Table"):
-    df = load_data(table_name)
-    st.dataframe(df)
+@st.cache_data
+def load_map(table_name):
+    st.write("Map of data engineering jobs in the US")
+    lat_lon_df = (
+        session.table(f"{table_name}")
+        .select(col("LATITUDE"), col("LONGITUDE"))
+        .filter(col("LATITUDE") != "unknown")
+    ).to_pandas()
+
+    lat_lon_df = lat_lon_df.astype({"LATITUDE": "float32", "LONGITUDE": "float32"})
+    lat_lon_df.drop_duplicates()
+    st.map(lat_lon_df)
+
+
+def folium_map(table_name: str) -> None:
+    st.write("Map of data engineering jobs in the US")
+    lat_lon_df = (
+        session.table(f"{table_name}")
+        .select(col("CITY"), col("LATITUDE"), col("LONGITUDE"))
+        .filter(col("LATITUDE") != "unknown")
+    ).to_pandas()
+
+    lat_lon_df = lat_lon_df.astype({"LATITUDE": "float32", "LONGITUDE": "float32"})
+    lat_lon_df.drop_duplicates()
+
+    # center on Liberty Bell, add marker
+    lat_lon_tuples = list(
+        zip(
+            lat_lon_df["CITY"].to_list(),
+            lat_lon_df["LATITUDE"].to_list(),
+            lat_lon_df["LONGITUDE"].to_list(),
+        )
+    )
+
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
+    for city, lat, long in lat_lon_tuples:
+        folium.Marker([lat, long], popup=city, tooltip=city).add_to(m)
+
+    # call to render Folium map in Streamlit
+    st_data = st_folium(m, width=725)
+
+
+def main() -> None:
+    table_name = "indeed_de_jobs_us"
+    with st.expander("See Table"):
+        df = load_data(table_name)
+        st.dataframe(df)
+
+    folium_map(table_name)
+
+
+if __name__ == "__main__":
+    main()
